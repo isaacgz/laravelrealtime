3e38aec7620c12cf3128881f72e8b546157557eb
Isaac Garcia <joelgarcia405@gmail.com>
2020-06-09 14:05:38 -0500

    Finaliza las apps realtime

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Events\MessageSent;
+use App\Events\GreetingSent;
+use App\User;
 use Illuminate\Http\Request;
 
 class ChatController extends Controller
@@ -51,6 +53,9 @@ class ChatController extends Controller
 
     public function greetReceived(Request $request, User $user)
     {
+        broadcast(new GreetingSent($user, "{$request->user()->name} greeted you"));
+        broadcast(new GreetingSent($request->user(), "You greeted {$user->name}"));
+
         return "Greeting {$user->name} from {$request->user()->name}";
     }
     
